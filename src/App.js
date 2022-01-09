349dc43dcd3e5d691ef481c8285452a4ffba4e33
JacobTTorres <jacobttorres@hotmail.com>
2022-01-09 15:46:40 -0600

    need to fix background color and may need to do break points

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./app.css";
 import "./index.css";
 
 import Header from "./components/header/Header";
+import Separator from "./components/separator/Separator";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Separator />
     </div>
   );
 }
---
+++ src/components/separator/Separator.js
@@ -0,0 +1,14 @@
+import "./separator.css";
+
+export default function Separator() {
+  return (
+    <section className="separatorContainer">
+      <div className="separator">
+        <h2 className="separatorTitle">
+          Learn how to turn more of your blog reader into customers through
+          conversion rate optimization.
+        </h2>
+      </div>
+    </section>
+  );
+}
